bcaa90c36f7ed1325a5efd33781e2889bd7a12a4
ajowyang <andrewowyang875@gmail.com>
2025-01-02 06:53:26 -0800

    completed data types and basic operations exercises

---
+++ ch1_intro_to_programming/basicOperations.py
@@ -0,0 +1,6 @@
+first_name = "Andrew"
+last_name = "Owyang"
+full_name = first_name + " " +  last_name
+print(full_name)
+
+print(5 + 10)
---
+++ ch1_intro_to_programming/datatypes.py
@@ -0,0 +1,12 @@
+names = ("Asta" , "Butterscotch", "Pudding", "Neptune", "Darwin")
+print(names)
+
+pets = {
+"Asta": "dog",
+"Butterscotch": "cat",
+"Pudding": "cat",
+"Neptune": "fish",
+"Darwin": "lizard",
+}
+
+print(pets)
